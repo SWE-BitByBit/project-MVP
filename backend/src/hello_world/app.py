73342118fcf281db522fdb8b864d79089133e8d8
Riccardo Manisi <riccardomanisi@gmail.com>
2026-04-07 10:29:42 +0200

    Task/sonarqube setup mvp (#10)
    * inserito progetto aws w makefile
    
    * fix minori
    
    * action da testare e risolto errore su unit test
    
    * Prova push per sonarqube
    
    * chore: add .sonar and coverage reports to gitignore
    
    * commit per il frontend
    
    * Prova push per sonarqube, aggiunta riga su frontend/sonar-project.properties
    
    * Aggiunto makefile per frontend
    
    * Aggiiunto file per configurazione automatica vscode
    
    ---------
    
    Co-authored-by: Rmanisi <riccardomansi@gmail.com>

--- backend/src/hello_world/app.py
+++ backend/src/hello_world/app.py
@@ -1,34 +1,30 @@
-from aws_lambda_powertools.event_handler import APIGatewayRestResolver
-from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.logging import correlation_paths
-from aws_lambda_powertools import Logger
-from aws_lambda_powertools import Tracer
-from aws_lambda_powertools import Metrics
-from aws_lambda_powertools.metrics import MetricUnit
+import json
+import boto3
+import os
 
-app = APIGatewayRestResolver()
-tracer = Tracer()
-logger = Logger()
-metrics = Metrics(namespace="Powertools")
+def response(status, body):
+        return {
+            "statusCode": status,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(body)
+        }
 
-@app.get("/hello")
-@tracer.capture_method
-def hello():
-    # adding custom metrics
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/metrics/
-    metrics.add_metric(name="HelloWorldInvocations", unit=MetricUnit.Count, value=1)
+def get_contact(table, event):
+    contact_id = event["pathParameters"]["contact_id"]
+    db_response = table.get_item(
+        Key={
+            "user_id": event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"],
+            "contact_id": contact_id
+        }
+    )
+    item = db_response.get("Item")
+    if not item:
+        return response(404, {"msg": "Not found"})
+    return response(200, item)
 
-    # structured log
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/logger/
-    logger.info("Hello world API - HTTP 200")
-    return {"message": "hello world"}
+def lambda_handler(event, context):
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ["TABLE_NAME"])
 
-# Enrich logging with contextual information from Lambda
-@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
-# Adding tracer
-# See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/tracer/
-@tracer.capture_lambda_handler
-# ensures metrics are flushed upon request completion/failure and capturing ColdStart metric
-@metrics.log_metrics(capture_cold_start_metric=True)
-def lambda_handler(event: dict, context: LambdaContext) -> dict:
-    return app.resolve(event, context)
+    if event.get("routeKey") ==  "GET /contacts/{contact_id}":
+        return get_contact(table, event)
