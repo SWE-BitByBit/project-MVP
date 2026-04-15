991f77816114782357527ac5614c5c1974660989
Rmanisi <riccardomansi@gmail.com>
2026-04-15 12:31:05 +0200

    Prova deploy su aws 2

--- backend/src/hello_world/app.py
+++ backend/src/hello_world/app.py
@@ -10,6 +10,7 @@ def response(status, body):
         }
 
 def get_contact(table, event):
+    """funzione per il fetch di un contatto"""
     contact_id = event["pathParameters"]["contact_id"]
     db_response = table.get_item(
         Key={
