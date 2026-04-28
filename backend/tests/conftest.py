a4001619e0f4e2c710db6539d82d120ffe6f3811
Rmanisi <riccardomansi@gmail.com>
2026-04-28 18:44:58 +0200

    aggiunta cartella test per resources/materials e tolto file conftest.py per il path

--- backend/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Aggiunge la directory 'src' al percorso di ricerca dei moduli Python.
-# Questo è necessario perché i moduli Lambda utilizzano import relativi al package
-# (es. 'from materials.xxx import ...') che devono essere risolti a partire da 'src/'.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
